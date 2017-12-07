30fe0074286307ff76ebdb7e042bd247ea821d85
zhanghaoran <haoranzeus@163.com>
2017-12-07 16:27:00 +0800

    [新增] 使用Adminlte做起来

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/adminlte/dist/css/AdminLTE.css'
+import './assets/zhr/test.css'
+import './assets/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css'
+import './assets/adminlte/bower_components/font-awesome/css/font-awesome.min.css'
+import './assets/adminlte/bower_components/Ionicons/css/ionicons.min.css'
+import './assets/adminlte/dist/css/AdminLTE.min.css'
+import './assets/adminlte/dist/css/skins/_all-skins.min.css'
+import './assets/adminlte/bower_components/morris.js/morris.css'
+import './assets/adminlte/bower_components/jvectormap/jquery-jvectormap.css'
+import './assets/adminlte/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css'
+import './assets/adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.css'
+import './assets/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css'
 
 Vue.config.productionTip = false
 
@@ -11,5 +23,8 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
+  http: {
+    root: '/root'
+  },
   components: { App }
 })
